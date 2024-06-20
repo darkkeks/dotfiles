43a1f2d9ae49ee620a0eba7f8a8b0bbfa1e6c3d3
darkkeks <darkkeks@rambler.ru>
2024-06-21 01:00:17 +0200

    [macos] Switch to arm64 jdk

--- nix-darwin/jdk.nix
+++ nix-darwin/jdk.nix
@@ -4,8 +4,8 @@ let
     pname = "yandex-jdk";
     version = "17.0.2";
     src = fetchzip {
-      url = "https://proxy.sandbox.yandex-team.ru/2720886299";
-      hash = "sha256-ile3n1d9ZX/H95qn3zEtLKkONe598xUkfYj8dcAxAJg";
+      url = "https://proxy.sandbox.yandex-team.ru/2720893567";
+      hash = "sha256-YDwhns99iq7WWIrfQz45Ktdh749w+ioVaFFFvbsZ2L4";
       extension = "tar.gz";
       stripRoot = false;
     };
